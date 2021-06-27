4b5e35161fe2e5d67297bd37f3356a347f0ced1f
peaceiris <30958501+peaceiris@users.noreply.github.com>
2021-06-28 02:32:29 +0900

    fix

--- images/ubuntu-20.04/provision_nonroot.sh
+++ images/ubuntu-20.04/provision_nonroot.sh
@@ -1,151 +1,64 @@
-# https://github.com/actions/virtual-environments/blob/win19/20210620.1/images/linux/scripts/helpers/etc-environment.sh
+#!/usr/bin/env bash
 
-function getEtcEnvironmentVariable {
-    variable_name="$1"
-    # remove `variable_name=` and possible quotes from the line
-    grep "^${variable_name}=" /etc/environment |sed -E "s%^${variable_name}=\"?([^\"]+)\"?.*$%\1%"
-}
+# fail on unset variables and command errors
+set -eu -o pipefail # -x: is for debugging
 
-function addEtcEnvironmentVariable {
-    variable_name="$1"
-    variable_value="$2"
-
-    echo "$variable_name=$variable_value" | sudo tee -a /etc/environment
-}
-
-function replaceEtcEnvironmentVariable {
-    variable_name="$1"
-    variable_value="$2"
-
-    # modify /etc/environemnt in place by replacing a string that begins with variable_name
-    sudo sed -i -e "s%^${variable_name}=.*$%${variable_name}=\"${variable_value}\"%" /etc/environment
-}
-
-function setEtcEnvironmentVariable {
-    variable_name="$1"
-    variable_value="$2"
-
-    if grep "$variable_name" /etc/environment > /dev/null; then
-        replaceEtcEnvironmentVariable $variable_name $variable_value
-    else
-        addEtcEnvironmentVariable $variable_name $variable_value
-    fi
-}
-
-function prependEtcEnvironmentVariable {
-    variable_name="$1"
-    element="$2"
-    # TODO: handle the case if the variable does not exist
-    existing_value=$(getEtcEnvironmentVariable "${variable_name}")
-    setEtcEnvironmentVariable "${variable_name}" "${element}:${existing_value}"
-}
-
-function appendEtcEnvironmentVariable {
-    variable_name="$1"
-    element="$2"
-    # TODO: handle the case if the variable does not exist
-    existing_value=$(getEtcEnvironmentVariable "${variable_name}")
-    setEtcEnvironmentVariable "${variable_name}" "${existing_value}:${element}"
-}
-
-function prependEtcEnvironmentPath {
-    element="$1"
-    prependEtcEnvironmentVariable PATH "${element}"
-}
-
-function appendEtcEnvironmentPath {
-    element="$1"
-    appendEtcEnvironmentVariable PATH "${element}"
-}
-
-# Process /etc/environment as if it were shell script with `export VAR=...` expressions
-#    The PATH variable is handled specially in order to do not override the existing PATH
-#    variable. The value of PATH variable read from /etc/environment is added to the end
-#    of value of the exiting PATH variable exactly as it would happen with real PAM app read
-#    /etc/environment
-#
-# TODO: there might be the others variables to be processed in the same way as "PATH" variable
-#       ie MANPATH, INFOPATH, LD_*, etc. In the current implementation the values from /etc/evironments
-#       replace the values of the current environment
-function  reloadEtcEnvironment {
-    # add `export ` to every variable of /etc/environemnt except PATH and eval the result shell script
-    eval $(grep -v '^PATH=' /etc/environment | sed -e 's%^%export %')
-    # handle PATH specially
-    etc_path=$(getEtcEnvironmentVariable PATH)
-    export PATH="$PATH:$etc_path"
-}
-
-# install Linuxbrew
-# cf. https://github.com/actions/virtual-environments/blob/win19/20210620.1/images/linux/scripts/installers/homebrew.sh
-# Install the Homebrew on Linux
+# Install Linuxbrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 echo "eval \$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" >> ~/.profile
 echo "eval \$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" >> ~/.bash_profile
 echo "eval \$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" >> ~/.bashrc
 brew doctor
 brew update
 
-# Update /etc/environemnt
-## Put HOMEBREW_* variables
-brew shellenv|grep 'export HOMEBREW'|sed -E 's/^export (.*);$/\1/' | sudo tee -a /etc/environment
-# add brew executables locations to PATH
-brew_path=$(brew shellenv|grep  '^export PATH' |sed -E 's/^export PATH="([^$]+)\$.*/\1/')
-prependEtcEnvironmentPath "$brew_path"
-setEtcEnvironmentVariable HOMEBREW_NO_AUTO_UPDATE 1
-setEtcEnvironmentVariable HOMEBREW_CLEANUP_PERIODIC_FULL_DAYS 3650
-
-# Validate the installation ad hoc
-echo "Validate the installation reloading /etc/environment"
-reloadEtcEnvironment
-
 # Install additional brew packages
 brew install \
-    ansible \
-    ansible-lint \
-    aquasecurity/trivy/trivy \
-    awscli \
-    bash \
-    clang-format \
-    cloudflare/cloudflare/cloudflared \
-    direnv \
-    gcc \
-    gh \
-    git \
-    git-lfs \
-    go \
-    golangci-lint \
-    goreleaser/tap/goreleaser \
-    hadolint \
-    helm \
-    hyperfine \
-    jq \
-    kind \
-    kubectl \
-    kustomize \
-    mage \
-    make \
-    minikube \
-    netlify-cli \
-    nkf \
-    node \
-    poetry \
-    pyenv \
-    qrencode \
-    rbenv \
-    shellcheck \
-    skaffold \
-    tree \
-    vercel-cli \
-    wget \
-    yamllint \
-    yarn \
-    zstd
+  ansible \
+  ansible-lint \
+  aquasecurity/trivy/trivy \
+  awscli \
+  bash \
+  clang-format \
+  cloudflare/cloudflare/cloudflared \
+  direnv \
+  gcc \
+  gh \
+  git \
+  git-lfs \
+  go \
+  golangci-lint \
+  goreleaser/tap/goreleaser \
+  hadolint \
+  helm \
+  hyperfine \
+  jq \
+  kind \
+  kubectl \
+  kustomize \
+  mage \
+  make \
+  minikube \
+  netlify-cli \
+  nkf \
+  node \
+  poetry \
+  pyenv \
+  qrencode \
+  rbenv \
+  shellcheck \
+  skaffold \
+  tree \
+  vercel-cli \
+  wget \
+  yamllint \
+  yarn \
+  zstd
 
 git lfs install
 npm i -g npm
 # create symlinks for zstd in /usr/local/bin
-find $(brew --prefix)/bin -name *zstd* -exec sudo sh -c 'ln -s {} /usr/local/bin/$(basename {})' ';'
+find "$(brew --prefix)/bin" -name "*zstd*" -exec sudo sh -c 'ln -s "$1" /usr/local/bin/$(basename "$1")' _ {} \;
 
 # Install deno
 curl -fsSL https://deno.land/x/install/install.sh | sh
--- images/ubuntu-20.04/provision_root.sh
+++ images/ubuntu-20.04/provision_root.sh
@@ -1,15 +1,59 @@
+#!/usr/bin/env bash
+
+# fail on unset variables and command errors
+set -eu -o pipefail # -x: is for debugging
+
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y \
+  build-essential \
+  jq \
+  libffi-dev \
+  libssl-dev \
+  python3 \
+  python3-dev \
+  python3-pip \
+  python3-venv \
+  wget
+
+# Install docker
+apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io
+groupadd docker
+gpasswd -a vagrant docker
+newgrp docker
+systemctl restart docker
+
+# Install docker-compose
+DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+export DOCKER_COMPOSE_VERSION
+curl -sL "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install google-cloud-sdk (gcloud)
+sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4 default-jdk
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update -y
+sudo apt-get install -y google-cloud-sdk
+
 # Install Google Chrome
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
-sudo echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update -y
 sudo apt-get install -y google-chrome-stable
 
 # Install ChromeDriver
-export CHROMEDRIVER_TEMPDIR=$(mktemp -d)
-wget -O ${CHROMEDRIVER_TEMPDIR}/LATEST_RELEASE http://chromedriver.storage.googleapis.com/LATEST_RELEASE
-export CHROMEDRIVER_LATEST_VERSION=$(cat ${CHROMEDRIVER_TEMPDIR}/LATEST_RELEASE)
-wget -O ${CHROMEDRIVER_TEMPDIR}/chromedriver.zip "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_LATEST_VERSION}/chromedriver_linux64.zip"
-sudo unzip ${CHROMEDRIVER_TEMPDIR}/chromedriver.zip chromedriver -d /usr/local/bin/
+CHROMEDRIVER_TEMPDIR=$(mktemp -d)
+export CHROMEDRIVER_TEMPDIR
+wget -O "${CHROMEDRIVER_TEMPDIR}/LATEST_RELEASE http://chromedriver.storage.googleapis.com/LATEST_RELEASE"
+CHROMEDRIVER_LATEST_VERSION=$(cat "${CHROMEDRIVER_TEMPDIR}/LATEST_RELEASE")
+export CHROMEDRIVER_LATEST_VERSION
+wget -O "${CHROMEDRIVER_TEMPDIR}/chromedriver.zip" "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_LATEST_VERSION}/chromedriver_linux64.zip"
+sudo unzip "${CHROMEDRIVER_TEMPDIR}/chromedriver.zip" chromedriver -d /usr/local/bin/
 
 # Install terraform
 TERRAFORM_VERSION=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r .current_version)

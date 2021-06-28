54f872d2af19a562137b1fb6cbde4b0c624197a6
peaceiris <30958501+peaceiris@users.noreply.github.com>
2021-06-28 18:24:33 +0900

    fix shellcheck

--- images/ubuntu-20.04/provision_nonroot.sh
+++ images/ubuntu-20.04/provision_nonroot.sh
@@ -147,9 +147,9 @@ git lfs install
 npm i -g npm
 # create symlinks for zstd in /usr/local/bin
 find "$(brew --prefix)/bin" -name "*zstd*" -exec sudo sh -c 'ln -s "$1" /usr/local/bin/$(basename "$1")' _ {} \;
-echo 'export PATH="/home/linuxbrew/.linuxbrew/opt/clang-format/bin:${PATH}"' >> ~/.bash_profile
+echo "export PATH=\"/home/linuxbrew/.linuxbrew/opt/clang-format/bin:\${PATH}\"" >> ~/.bash_profile
 
 # Install deno
 curl -fsSL https://deno.land/x/install/install.sh | sh
-echo 'export PATH="${HOME}/.deno/bin:${PATH}"' >> ~/.profile
-echo 'export PATH="${HOME}/.deno/bin:${PATH}"' >> ~/.bash_profile
+echo "export PATH=\"\${HOME}/.deno/bin:\${PATH}\"" >> ~/.profile
+echo "export PATH=\"\${HOME}/.deno/bin:\${PATH}\"" >> ~/.bash_profile

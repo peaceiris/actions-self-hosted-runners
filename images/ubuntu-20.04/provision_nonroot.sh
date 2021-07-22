6b0427c8bfe384a812ecd811be476c8a1efc799e
Shohei Ueda <30958501+peaceiris@users.noreply.github.com>
2021-07-22 09:39:04 +0900

    Remove clang-format

--- images/ubuntu-20.04/provision_nonroot.sh
+++ images/ubuntu-20.04/provision_nonroot.sh
@@ -108,7 +108,6 @@ brew install \
   ansible-lint \
   aquasecurity/trivy/trivy \
   bash \
-  clang-format \
   direnv \
   gcc \
   gh \
@@ -147,7 +146,6 @@ git lfs install
 npm i -g npm
 # create symlinks for zstd in /usr/local/bin
 find "$(brew --prefix)/bin" -name "*zstd*" -exec sudo sh -c 'ln -s "$1" /usr/local/bin/$(basename "$1")' _ {} \;
-echo "export PATH=\"/home/linuxbrew/.linuxbrew/opt/clang-format/bin:\${PATH}\"" >> ~/.bash_profile
 
 # Install deno
 curl -fsSL https://deno.land/x/install/install.sh | sh
